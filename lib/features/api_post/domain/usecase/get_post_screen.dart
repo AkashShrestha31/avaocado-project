ba59fd45dbb071ad97ded3ecd5ef78cf4cb5d48a
AkashShrestha <aakashshrestha31@gmail.com>
2023-09-17 23:34:34 +0545

    small changes

--- lib/features/api_post/domain/usecase/get_post_screen.dart
+++ lib/features/api_post/domain/usecase/get_post_screen.dart
@@ -1,6 +1,6 @@
 import 'package:avocado/features/api_post/data/model/post_model.dart';
 import 'package:avocado/features/api_post/domain/repository/post_repository.dart';
-import 'package:avocado/features/usecase.dart';
+import 'package:avocado/core/usecase/usecase.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
